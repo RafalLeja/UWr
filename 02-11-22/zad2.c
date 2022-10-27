ef95c4e2406fa9bb212dab1f8777d8569e3fcd1d
RafalLeja <rafal@leja.org.pl>
2022-10-27 13:49:35 +0200

    lekcja 27-10

---
+++ 02-11-22/zad2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int nastepna(char wzorzec[], char tekst[], int poczatkowa){
+
+}
+
+void raportuj(char wzorzec[], char tekst[], int nr_linii){
+
+}
+
+int main(int argc, char const *argv[])
+{
+    if(argc > 2){
+        printf("podajesz zły argument")
+        return 0;
+    }
+    return 0;
+}
