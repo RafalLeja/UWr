7b674cbb3ccef757285973bf2091a650eca5bcc4
Maciej Stys <maciejstys123@gmail.com>
2022-11-30 19:53:41 +0100

    3/4 poprawne

--- lista5/zadM.c
+++ lista5/zadM.c
@@ -3,8 +3,9 @@
 
 int main(){
 
-for(long long d = 0; d < 100000; d++){
-    long long dk = d * d, a = 0, b = 0, c = 0;
+    long long d, a = 0, b = 0, c = scanf("%lld", &d);
+    long long dk = d * d;
+
     for(long long x = 1; x < d; x++){
         for(long long y = 1; y < d; y++){
             long long zk = dk - x*x - y*y;
@@ -21,11 +22,10 @@ for(long long d = 0; d < 100000; d++){
     }
 
     if(a == 0){
-        printf("%lld : BRAK\n", d);
+        printf("BRAK");
     }
     else{
-        printf("%lld : %lld %lld %lld\n", d, c, b ,a);
+        printf("%lld %lld %lld", c, b ,a);
     }
-}
-return 0;
+    return 0;
 }
