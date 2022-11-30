9fb2e1114b0e3e8f2568b87e2e730602fb10fa63
Maciej Stys <maciejstys123@gmail.com>
2022-11-30 19:51:36 +0100

    testy 3/4

--- lista5/zadM.c
+++ lista5/zadM.c
@@ -3,29 +3,29 @@
 
 int main(){
 
-    long long d, a = 0, b = 0, c = scanf("%lld", &d);
-    long long dk = d * d;
-
-    for(int x = d - 1; x > 0; x--){
-        for(int y = d - 1; y > 0; y--){
-            long long zk = 0;
-            if(dk - x*x - y*y){
-                zk = dk - x*x - y*y;
+for(long long d = 0; d < 100000; d++){
+    long long dk = d * d, a = 0, b = 0, c = 0;
+    for(long long x = 1; x < d; x++){
+        for(long long y = 1; y < d; y++){
+            long long zk = dk - x*x - y*y;
+            if(zk <= 0){
+                break;
             }
-            int z = sqrt(zk);
-            if(zk && zk - z*z == 0){
-                if(z * y * x > a * b * c){
+            long long z = sqrt(zk);
+            if(zk - z*z == 0){
+                if(x * y * z > a * b * c){
                     a = x; b = y; c = z;
                 }
             }
         }
-    }   
+    }
 
-    if(!a){
-        printf("BRAK");
+    if(a == 0){
+        printf("%lld : BRAK\n", d);
     }
     else{
-        printf("%lld %lld %lld", a, b ,c);
+        printf("%lld : %lld %lld %lld\n", d, c, b ,a);
     }
-    return 0;
+}
+return 0;
 }
