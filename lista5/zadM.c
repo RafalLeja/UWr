0497cb4163f2b220ce8adc1357bfd4904a348e75
RafalLeja <rafal@leja.org.pl>
2022-12-01 12:14:15 +0100

    kurwa 10/10

--- lista5/zadM.c
+++ lista5/zadM.c
@@ -1,13 +1,35 @@
 #include<stdio.h>
 #include<math.h>
 
+void sort(long long *a, long long *b, long long *c){
+    long long temp;
+    if (*c > *b)
+    {
+        temp = *c;
+        *c = *b;
+        *b = temp;
+    }
+    if (*b > *a)
+    {
+        temp = *b;
+        *b = *a;
+        *a = temp;
+    }
+    if (*c > *b)
+    {
+        temp = *c;
+        *c = *b;
+        *b = temp;
+    }
+}
+
 int main(){
 
     long long d, a = 0, b = 0, c = scanf("%lld", &d);
     long long dk = d * d;
 
     for(long long x = sqrt(dk/4); x < d; x++){
-        for(long long y = 1; y < d; y++){
+        for(long long y = x; y < d; y++){
             long long zk = dk - x*x - y*y;
             if(zk <= 0){
                 break;
@@ -25,6 +47,7 @@ int main(){
         printf("BRAK");
     }
     else{
+        sort(&c, &b, &a);
         printf("%lld %lld %lld", c, b ,a);
     }
     return 0;
