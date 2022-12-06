4865d6a2629993d6f3e083be2fd59a662f93823c
Maciej Stys <maciejstys123@gmail.com>
2022-12-06 23:05:42 +0100

    zad2/lista6

--- maciej/lista6/zad2/zad2.c
+++ maciej/lista6/zad2/zad2.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 typedef struct Ksiazka_{
     char autor[100];
     char tytul[100];
-    char rok[12];
-    char strony[12];
+    int rok;
+    int strony;
 }Ksiazka;
 
-int main(){
+int main(int argc, char* argv[]){
     Ksiazka ksiazka;
     FILE* plik;
 
-    plik = fopen("test.txt", "r");
+    plik = fopen(argv[argc - 1], "r");
 
-    fgets(ksiazka.autor, 100, plik);
-    fgets(ksiazka.tytul, 100, plik);
-    fgets(ksiazka.rok, 12, plik);
-    fgets(ksiazka.strony, 12, plik);
+    while(fgets(ksiazka.autor, 100, plik) != NULL){
+        fgets(ksiazka.tytul, 100, plik);
+        fscanf("%d %d", ksiazka.rok, ksiazka.strony);
 
-    printf("%s %s %s %s", ksiazka.autor, ksiazka.tytul, ksiazka.rok, ksiazka.strony);
-    fflush(stdout);
+        printf("%s %s %d %d\n", ksiazka.autor, ksiazka.tytul, ksiazka.rok, ksiazka.strony);
+    }
 
     fclose(plik);
 
