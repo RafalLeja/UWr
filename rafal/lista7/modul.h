ec5cdb22e2077163c8f7cacfae0fd53a761c7810
RafalLeja <rafal@leja.org.pl>
2022-12-07 12:21:56 +0100

    kurwa git nie chce trackowac/ jednak tylko nie zapisaem pliu

--- rafal/lista7/modul.h
+++ rafal/lista7/modul.h
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-typedef struct Deque
+typedef struct DequeS
 {
     int* store;
 } * Deque;
 
 Deque new_Deque(){
-
+    Deque* que = malloc(sizeof(Deque));
+    que->data = malloc(capacity * sizeof(int));
+    que->size = 0;
+    que->start = 0;
+    que->end = -1;
+    que->capacity = capacity;
+    return que;
 }
 
 void del(Deque que){
@@ -35,4 +41,12 @@ int pop_left(Deque que){
 
 int pop_right(Deque que){
     
+}
+
+int len(Deque que){
+
+}
+
+int size(Deque que){
+
 }
--- rafal/lista7/open.h
+++ rafal/lista7/open.h
@@ -14,10 +14,10 @@ typedef struct deque_s {
 
 // Function prototypes
 Deque new_Deque();
-void del(Deque* deque);
+void del(Deque deque);
 int size(Deque* deque);
 int isEmpty(Deque deque);
-int peek_left(Deque* deque);
+int peek_left(Deque deque);
 int deque_back(Deque* deque);
 void deque_push_front(Deque* deque, int value);
 void deque_push_back(Deque* deque, int value);
@@ -36,9 +36,10 @@ Deque new_Deque() {
 }
 
 // Free the memory used by the deque
-void del(Deque* deque) {
-  free(deque->store);
-  free(deque);
+void del(Deque deque) {
+  free(deque.store);
+  Deque *ptr = (deque_s*) deque;
+  free(ptr);
 }
 
 // Return the number of elements in the deque
@@ -60,17 +61,52 @@ int peek_left(Deque* deque) {
 }
 
 // Return the value of the back element in the deque
-int* deque_back(Deque* deque) {
-  if (isEmpty(deque)) {
+int* deque_back(Deque deque) {
+  if (isEmpty(&deque)) {
     printf("pusta tablica\n");
     return NULL;
   }
-  return &deque->data[deque->start];
+  return &deque.store[deque.start];
 }
 
 // Push a new value to the front of the deque
 void deque_push_front(Deque* deque, int value) {
   if (deque->size == deque->capacity) {
-    printf("Error: deque is full
+    printf("Error: deque is full\n");
+    exit(1);
+  }
+  deque->start = (deque->start - 1 + deque->capacity) % deque->capacity;
+  deque->store[deque->start] = value;
+  deque->size++;
+}
 
+// Push a new value to the back of the deque
+void deque_push_back(Deque* deque, int value) {
+  if (deque->size == deque->capacity) {
+    printf("Error: deque is full\n");
+    exit(1);
+  }
+  deque->end = (deque->end + 1) % deque->capacity;
+  deque->data[deque->end] = value;
+  deque->size++;
+}
 
+// Remove the front element from the deque
+void deque_pop_front(Deque* deque) {
+  if (deque_empty(deque)) {
+    printf("Error: deque is empty\n");
+    exit(1);
+  }
+  deque->start = (deque->start + 1) % deque->capacity;
+  deque->size--;
+}
+
+// Remove the back element from the deque
+void deque_pop_back(Deque* deque) {
+  if (deque_empty(deque)) {
+    printf("Error: deque is empty\n");
+    exit(1);
+  }
+  deque->end = (deque->end - 1 + deque->capacity) % deque->capacity;
+  deque->size--;
+}
