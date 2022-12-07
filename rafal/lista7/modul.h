fd5aa90169e8f2941404e54bbda0ce101eb6cf09
RafalLeja <rafal@leja.org.pl>
2022-12-07 11:54:51 +0100

    dodanie kodu z chatGPT

---
+++ rafal/lista7/modul.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+typedef struct Deque
+{
+    int* store;
+} * Deque;
+
+Deque new_Deque(){
+
+}
+
+void del(Deque que){
+
+}
+
+int push_left(Deque que, int offset){
+
+}
+
+int push_right(Deque que, int offset){
+
+}
+
+int* peek_left(Deque que){
+
+} 
+
+int* peek_right(Deque que){
+    
+}
+
+int pop_left(Deque que){
+
+}
+
+int pop_right(Deque que){
+    
+}
