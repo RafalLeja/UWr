c9f48a55ae6625ab148215749e7578054bb052e2
RafalLeja <rafal@leja.org.pl>
2022-12-07 17:25:15 +0100

    zadanie drugie sie kompiluje

--- rafal/lista7/zad2R.c
+++ rafal/lista7/zad2R.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-#include "modul.c"
+#include "modul.h"
 
 int main()
 {
     Deque tab = new_Deque();
+    printf("size = %d\n", tab.capacity);
+    push_left(tab, 3);
+    push_left(tab, 2);
+    push_left(tab, 1);
+    push_right(tab, 4);
     printf("size = %d\n", tab.size);
-    printf("aa");
+    printf("%d\n", *peek_left(tab));
     return 0;
 }
