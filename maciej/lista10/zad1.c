443624a2bbbc1f831b9b63c41939b0709af60ec1
Maciej Stys <maciejstys123@gmail.com>
2023-01-04 19:42:16 +0100

    dodanie funkcji: stworz element, polacz z lista, zwolnij pamiec, wyswietl

--- maciej/lista10/zad1.c
+++ maciej/lista10/zad1.c
@@ -1,17 +1,53 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 typedef struct PtList{
     float x;
     float y;
     float z;
-    char nazawa[47];
+    char nazwa[47];
     struct PtList *next;
-}ptlist;
+}PtList;
 
+void polacz(PtList** lista, PtList* nowyElement){
+    if(*lista == NULL){
+        *lista = nowyElement;
+    }
+
+    else{
+        for(PtList* licznik = *lista; licznik != NULL; licznik = licznik->next){
+            if(licznik->next == NULL){
+                licznik->next = nowyElement;
+                break;
+            }
+        }
+    }
+}
+
+void stworz(PtList** lista, float a, float b, float c, char nazwaa[47]){
+    PtList* nowy = (PtList*)malloc(sizeof(PtList));
+    nowy->x = a;
+    nowy->y = b;
+    nowy->z = c;
+    strcpy(nowy->nazwa, nazwaa);
+    nowy->next = NULL;
+    polacz(lista, nowy);
+}
+
+void wyswietl(PtList* lista){
+    for(PtList* licznik = lista; licznik != NULL; licznik = licznik->next){
+        printf("%f %f %f %s\n", licznik->x, licznik->y, licznik->z, licznik->nazwa);
+    }
+}
+
+void pamiec(PtList** lista){
+    for(PtList* licznik = *lista; licznik != NULL; licznik = licznik->next){
+        free(licznik);
+    }
+}
 
 int main(int argc, char* argv[]){
-    struct PtList *lis;
-    lis = (struct PtList *) malloc(sizeof(struct PtList));
 
     if(argc < 2){
         printf("Niepoprawna liczba argumentow wywolania");
@@ -25,9 +61,18 @@ int main(int argc, char* argv[]){
         return 2;
     }
 
+    PtList* lista = NULL; 
+    float f1, f2, f3;
+    char nazwa[47], linia[256];
 
+    while(fgets(linia, sizeof(linia), fp)){
+        sscanf(linia, "%f %f %f  %[^\n]", &f1, &f2, &f3, nazwa);
+        stworz(&lista, f1, f2, f3, nazwa);
+    }
+    fclose(fp);
 
-
+    wyswietl(lista);
+    pamiec(&lista);
 
     return 0;
 }
