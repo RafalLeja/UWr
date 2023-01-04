b225d46964a7d5a503edb81a04f5e6a52a30b777
RafalLeja <rafal@leja.org.pl>
2023-01-04 19:57:56 +0100

    ostatnie zmiany do fraktali

--- rafal/lista9/zad1R.c
+++ rafal/lista9/zad1R.c
@@ -79,7 +79,7 @@ int main(int argc, char const *argv[]){
 				
 	}
 
-	fprintf(plik, "<html>\n<body>\n<svg width=\"%f\" height=\"%f\" viewBox=\"%f %f\">\n", WIDTH, HEIGHT, WIDTH, HEIGHT);
+	fprintf(plik, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"%f\" height=\"%f\" viewBox=\"%f %f\">", WIDTH, HEIGHT, WIDTH, HEIGHT);
 
 	switch (draw)
 	{
@@ -121,7 +121,7 @@ int main(int argc, char const *argv[]){
 			break;
 	}
 
-	fprintf(plik, "</svg>\n </body>\n </html>");
+	fprintf(plik, "</svg>");
 	fclose(plik);
 
 }
