e36c07ffd483037a7bf7176669d296631baad624
Maciej Stys <maciejstys123@gmail.com>
2023-01-06 17:00:15 +0100

    PROJEKT dodanie funkcji z wlasciwosciami trasy: czas start/stop, calkowity czas, max/min wysokosc, srednia szybkosc, dystans

--- maciej/projekt/main.c
+++ maciej/projekt/main.c
@@ -1,13 +1,56 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<math.h>
+#include<time.h>
+
+#define Pi 3.141593
+#define deg2rad(deg) ((deg) * Pi / 180.0)
+#define earthRadius 6371.0
 
 typedef struct point{
     double lat;
     double lon;
+    double ele;
     char time[20];
     struct point* next;
 }point;
+
+typedef struct properties{
+    char start[19];
+    char end[19];
+    int totalTime;
+    //double maxSpeed;
+    double maxHeight;
+    double minHeight;
+    double averageSpeed;
+    double distance;
+    // double bestRate;
+    // double averageRate;
+}properties;
+
+// typedef struct kilometer{
+//     int number;
+//     double startLat;
+//     double startLon;
+//     double endLat;
+//     double endLon;
+//     char start[19];
+//     char end[19];
+// }kilometer;
+
+double dist(double lat1, double lon1, double lat2, double lon2){
+    double dLat = deg2rad(lat2 - lat1);
+    double dLon = deg2rad(lon2 - lon1);
+
+    lat1 = deg2rad(lat1);
+    lat2 = deg2rad(lat2);
+
+    double a = sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat1) * cos(lat2); 
+    double c = 2 * atan2(sqrt(a), sqrt(1-a)); 
+    
+    return earthRadius * c * 1000;
+}
 
 void join(point** list, point* newElement){
     if(*list == NULL){
@@ -23,10 +66,11 @@ void join(point** list, point* newElement){
     }
 }
 
-void createPoint(point** lista, double lat, double lon, char time[20]){
+void createPoint(point** lista, double lat, double lon, double ele, char time[20]){
     point* new = (point*)malloc(sizeof(point));
     new->lat = lat;
     new->lon = lon;
+    new->ele = ele;
     strcpy(new->time, time);
     new->next = NULL;
     join(lista, new);
@@ -48,6 +92,14 @@ point* getData(char* filePath){
         if (strncmp(line, "<trkpt ", 7) == 0){
             double lat, lon;
             sscanf(line, "<trkpt lat=\"%lf\" lon=\"%lf\">", &lat, &lon);
+
+            while(strncmp(line, "<ele>", 5) != 0){
+                fgets(line, sizeof(line), fp);
+                sscanf(line, " %[^\n]", line);
+            }
+
+            double ele;
+            sscanf(line, "<ele>%lf<", &ele);
         
             while(strncmp(line, "<time>", 6) != 0){
                 fgets(line, sizeof(line), fp);
@@ -57,18 +109,78 @@ point* getData(char* filePath){
             char time[20];
             sscanf(line, "<time>%19[^<]</time>", time);
 
-            createPoint(&points, lat, lon, time);
+            createPoint(&points, lat, lon, ele, time);
         }
     }
 
     return *start;
 }
 
-void wyswietl(point* list){
+void show(point* list){
     for(point* counter = list; counter != NULL; counter = counter->next){
-        printf("%f %f %s\n", counter->lat, counter->lon, counter->time);
+        printf("%f %f %f %s\n", counter->lat, counter->lon, counter-> ele, counter->time);
     }
 }
+
+int time2sec(char timeStr[19]) {
+    struct tm tm = {0};
+    sscanf(timeStr, "%4d-%2d-%2dT%2d:%2d:%2d", &tm.tm_year, &tm.tm_mon, &tm.tm_mday, &tm.tm_hour, &tm.tm_min, &tm.tm_sec);
+    tm.tm_year -= 1900;
+    tm.tm_mon--;
+    time_t time = mktime(&tm);
+
+    return (int) time;
+}
+
+properties getProperties(point* list){
+    properties prop;
+
+    int line = 1;
+    double coords[4];
+    double distance = 0;
+    point* counter = list;
+
+
+    int timeStart = time2sec(counter->time);
+    int timeEnd;
+    coords[0] = counter->lat;
+    coords[1] = counter->lon;
+    prop.maxHeight = counter->ele;
+    prop.minHeight = counter->ele;
+    strcpy(prop.start, counter->time);
+    counter = counter->next;
+
+    for(counter; counter != NULL; counter = counter->next){
+        if(line % 2 == 0){
+            coords[0] = counter->lat;
+            coords[1] = counter->lon;
+        }
+        else{
+            coords[2] = counter->lat;
+            coords[3] = counter->lon;
+        }
+        distance += dist(coords[0], coords[1], coords[2], coords[3]);
+
+        if(counter->ele > prop.maxHeight){
+            prop.maxHeight = counter->ele;
+        }
+        else if(counter->ele < prop.minHeight){
+            prop.minHeight = counter->ele;
+        }
+
+        timeEnd = time2sec(counter->time);
+        strcpy(prop.end, counter->time);
+
+        line++;
+    }
+
+    prop.totalTime = timeEnd - timeStart;
+    prop.distance = distance;
+    prop.averageSpeed = prop.distance / prop.totalTime * 3.6;
+
+    return prop;
+}
+
 
 int main(int argc, char* argv[]){
     if(argc < 2){
@@ -78,7 +190,12 @@ int main(int argc, char* argv[]){
 
     point* listOfPoints = getData(argv[1]);
 
-    wyswietl(listOfPoints);
+    properties prop;
+
+    // show(listOfPoints);
+    prop = getProperties(listOfPoints);
+
+    printf("%s\n%s\n%d\n%f\n%f\n%f\n%f", prop.start, prop.end, prop.totalTime, prop.maxHeight, prop.minHeight, prop.averageSpeed, prop.distance);
     
 
     return 0;
