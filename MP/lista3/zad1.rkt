36b848aed6994d8cbb14f2f4855456c2bd0e2869
RafalLeja <rafal@leja.org.pl>
2023-03-14 11:25:19 +0100

    fix zad1 lista3 MP

--- MP/lista3/zad1.rkt
+++ MP/lista3/zad1.rkt
@@ -3,14 +3,20 @@
 ;1
 '(( car ( a . b ) ) (* 2) )
 
-(list `(car (a . b)) `(* 2))
+(list (list `car (cons `a `b)) (list `* 2))
+
+;(list `(car (a . b)) `(* 2))
 
 ;2
 `( ,( car '( a . b ) ) ,(* 2) )
 
-(list (car '(a . b)) (* 2))
+(list (car (cons `a `b)) (* 2))
+
+;(list (car '(a . b)) (* 2))
 
 ;3
 '((+ 1 2 3) (cons) (cons a b) )
 
-(list '(+ 1 2 3) '(cons) '(cons a b))
+(list (list `+ `1 `2 `3) (list `cons) (list `cons `a `b))
+
+;(list '(+ 1 2 3) '(cons) '(cons a b))
