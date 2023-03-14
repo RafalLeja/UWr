507bd992664590a614c8103c759fb3d9bf75887f
RafalLeja <rafal@leja.org.pl>
2023-03-14 11:45:56 +0100

    fix zad3 lista3 MP

--- MP/lista3/zad3.rkt
+++ MP/lista3/zad3.rkt
@@ -3,11 +3,11 @@
 ; (+ 1 (* 1 2)) == 3
 (( lambda ( x y ) (+ x (* x y ) ) ) 1 2)
 
-;#<procedure> | (x => x)(x => x)
+;#<procedure> | (x => x)(x => x) = (x => x) = x
 (( lambda ( x ) x ) ( lambda ( x ) x ) )
 
-;#<procedure> | (x => X(x))(x => x)
+;#<procedure> | (x => (x x))(x => x) = ((x => x) (x => x)) = (x => x) = x
 (( lambda ( x ) ( x x ) ) ( lambda ( x ) x ) )
 
-;inf | (x => X(x))(x => X(x))
+;inf | (x => X(x))(x => X(x)) => ...
 (( lambda ( x ) ( x x ) ) ( lambda ( x ) ( x x ) ) )
