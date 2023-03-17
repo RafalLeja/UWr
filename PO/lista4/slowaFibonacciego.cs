030b28b0d1220ac2edd328aefdc5fa30e326a9d5
Unknown <rafal@leja.org.pl>
2023-03-17 16:31:39 +0100

    slowa lista4 PO

--- PO/lista4/slowaFibonacciego.cs
+++ PO/lista4/slowaFibonacciego.cs
@@ -8,7 +8,7 @@ using System.Collections;
 
 namespace fibonacci
 {
-    class Slowo
+    public class Slowo
     {
         public string val;
 
@@ -38,10 +38,87 @@ namespace fibonacci
         }
     }
 
-    class SlowaFibonacciego : IEnumarable
+    public class SlowaFibonacciego : IEnumerable
     {
         private Slowo[] _slowa;
-        public SlowaFibonacciego(int n);
+        public SlowaFibonacciego(int n)
+        {
+           _slowa = new Slowo[n];
 
+           for (int i = 0; i < n; i++)
+           {
+                _slowa[i] = new Slowo(i+1);
+           }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator) GetEnumerator();
+        }
+
+        public SlowaEnum GetEnumerator()
+        {
+            return new SlowaEnum(_slowa);
+        }
     }
+
+    public class SlowaEnum : IEnumerator
+    {
+        public Slowo[] _slowa;
+
+        int position = -1;
+
+        public SlowaEnum(Slowo[] list)
+        {
+            _slowa = list;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < _slowa.Length);
+
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public Slowo Current
+        {
+            get
+            {
+                try
+                {
+                    return _slowa[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+
+    public class Program
+    {
+        public void Main()
+        {
+            SlowaFibonacciego slowa = new SlowaFibonacciego(6);
+            foreach (Slowo s in slowa)
+            {
+                System.Console.WriteLine(s);
+            }
+        }
+    }
+
 }
