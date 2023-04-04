c16e3c1e9707f6e3cb0853f3e7b19e2c2cc97e1e
RafalLeja <rafal@leja.org.pl>
2023-04-04 13:57:51 +0200

    progres table-select projekt1 MP

--- MP/projekt/bazy-danych.rkt
+++ MP/projekt/bazy-danych.rkt
@@ -106,6 +106,52 @@
 (define-struct eq2-f (name name2))
 (define-struct lt-f (name val))
 
-(define (form-interpreter form)
+(define (intersection lst1 lst2)
+  (define (itr l)
+    (if (not (check-duplicates l))
+      '()
+      (cons (check-duplicates l) (itr (remove (check-duplicates l) l)))))
+  (itr (append lst1 lst2)))
+
+(define (union lst1 lst2)
+  (remove-duplicates (append lst1 lst2)))
+
+(define (select-values name val table)
+  (define idx (get-idx name table))
+  (define (itr l)
+    (if (null? l)
+      l
+      (if (equal? val (list-ref (car l) idx))
+        (cons (car l) (itr (cdr l)))
+        (itr (cdr l)))))
+  (itr (table-rows table))) 
+
+(define (select-lower-values name val table)
+  (define idx (get-idx name table))
+  (define (itr l)
+    (if (null? l)
+      l
+      (if (type-comp (list-ref (car l) idx) val)
+        (cons (car l) (itr (cdr l)))
+        (itr (cdr l)))))
+  (itr (table-rows table))) 
+
+(define (select-equal-values name name2 table)
+  (define idx (get-idx name table))
+  (define idx2 (get-idx name2 table))
+  (define (itr l)
+    (if (null? l)
+      l
+      (if (equal? (list-ref (car l) idx2) (list-ref (car l) idx))
+        (cons (car l) (itr (cdr l)))
+        (itr (cdr l)))))
+  (itr (table-rows table)))
+
+(define (form-interpreter form table)
     (cond 
-        []))
+        [(and-f? form) (intersection (form-interpreter (and-f-l form) table) (form-interpreter (and-f-r form) table))]
+        [(or-f? form) (union (form-interpreter (or-f-l form) table) (form-interpreter (or-f-r form) table))]
+        [(not-f? form) (remove* (form-interpreter (not-f-e form) table) (table-rows table))]
+        [(eq-f? form) (select-values (eq-f-name form) (eq-f-val form) table)]
+        [(eq2-f? form) (select-equal-values (eq2-f-name form) (eq2-f-name2 form) table)]
+        [(lt-f? form) (select-lower-values (lt-f-name form) (lt-f-val form) table)]))
