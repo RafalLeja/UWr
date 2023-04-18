f7751d3bcca962a9a708872ee844067c5ec52a28
RafalLeja <rafal@leja.org.pl>
2023-04-18 19:36:02 +0200

    poczatek okienka edycji lista7 PO

--- PO/lista7/lista7/src/App.java
+++ PO/lista7/lista7/src/App.java
@@ -1,20 +1,7 @@
-import java.awt.*;
 
-import javax.swing.*;
 
 public class App {
   public static void main(String[] args) throws Exception {
 
-    JFrame frame = new JFrame("Edycja książki");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    Container kontener = frame.getContentPane();
-    GridLayout layout = new GridLayout(4, 2);
-    kontener.setLayout(layout);
-
-    JLabel autor_etykieta = new JLabel("Autor");
-    kontener.add(autor_etykieta);
-    JTextField autor = new JTextField(this.autor, 40);
-    kontener.add(autor);
   }
 }
