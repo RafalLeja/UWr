35b068f95f433e029a9eff3bf3719b8c9a129347
RafalLeja <rafal@leja.org.pl>
2023-04-30 23:11:54 +0200

    zad1,3 lista9 PO

--- PO/lista9/Function.rb
+++ PO/lista9/Function.rb
@@ -25,8 +25,26 @@ class Function
     @x = a
     ((b-a)/@e).round().times do
       @sum += @proc.call(@x) * @e
-    @x += @e
+      @x += @e
     end
     @sum
+  end
+
+  def deriv(x)
+    @h = 0.0001
+    ((self.value(x+@h)-self.value(x))/@h).round(3)
+  end
+
+  def plot(a,b,f)
+    File.write(f, "reset;\n$Points << EOD\n")
+    @e = 0.1
+    @x = a
+    ((b-a)/@e).round().times do
+      File.write(f, " " + @x.to_s + 
+        " " + self.value(@x).to_s + "\n", mode: "a")
+      @x += @e
+    end
+    File.write(f, "EOD\nplot $Points with lines\n", mode: "a")
+    File.write(f, "pause mouse close", mode: "a")
   end
 end
--- PO/lista9/Main.rb
+++ PO/lista9/Main.rb
@@ -1,7 +1,21 @@
+=begin
+ * Rafał Leja 340879
+ * PO lista9 zad 1 i 3
+ * >ruby "~\...\Main.rb"
+ * gnuplot .\{Nazwa Pliku}.gp
+=end
+
+
 require_relative "Function"
 
-  blok = proc { | x | 1 }
-  funkcja = Function.new(blok)
-  puts funkcja.value(5)
-  puts funkcja.zero(-5,5,1)
-  puts funkcja.field(0,5)
+  kwadratowa = Function.new(proc { | x | x**2 })
+
+  puts kwadratowa.value(5)
+  puts kwadratowa.zero(-5,5,1)
+  puts kwadratowa.field(0,5)
+  puts kwadratowa.deriv(2)
+
+  wykresowa = Function.new(
+    proc { | x | (x*Math.sin(x))**1/2 })
+
+  wykresowa.plot(-50,50,"wykres.gp")
