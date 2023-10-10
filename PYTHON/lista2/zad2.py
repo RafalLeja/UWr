4ddeea5c30ba718b7079f34929020784f1846e3a
RafalLeja <rafal@leja.org.pl>
2023-10-10 13:52:40 +0200

    koniec zad 2 python

--- PYTHON/lista2/zad2.py
+++ PYTHON/lista2/zad2.py
@@ -2,6 +2,10 @@
 # zadanie 2 lista 2 "sudan"
 # 7.10.2023
 
+mem = {
+    "0|0|0" : 0
+}
+
 def sudan(n, x, y):
     if n == 0:
         return x + y
@@ -9,4 +13,19 @@ def sudan(n, x, y):
         return x
     return sudan(n-1, sudan(n, x, y-1), sudan(n, x, y-1) + y)
 
+def sudanM(n, x, y):
+    key = str(n) + "|" + str(x) + "|" + str(y)
+    if key in mem:
+        return mem[key]
+    
+    if n == 0:
+        mem[key] = x + y
+        return x + y
+    if y == 0:
+        mem[key] = x
+        return x
+    mem[key] = sudan(n-1, sudan(n, x, y-1), sudan(n, x, y-1) + y)
+    return mem[key]
+
+print(sudanM(1, 1, 3))
 print(sudan(1, 1, 3))
