bfebd32130b0744cdcefe208fe0a7e4ee9beafc6
RafalLeja <rafal@leja.org.pl>
2023-10-13 14:15:41 +0200

    komentarz do zad3/lista2

--- PYTHON/lista2/zad2.py
+++ PYTHON/lista2/zad2.py
@@ -15,18 +15,27 @@ def sudan(n, x, y):
     return sudan(n-1, sudan(n, x, y-1), sudan(n, x, y-1) + y)
 
 def sudanM(n, x, y):
+    #print("[ sudanM(" + str(n) + ", " + str(x) + ", " + str(y) + ")\n\t")
     key = str(n) + "|" + str(x) + "|" + str(y)
     if key in mem:
         return mem[key]
-    
     if n == 0:
         mem[key] = x + y
         return x + y
     if y == 0:
         mem[key] = x
         return x
-    mem[key] = sudan(n-1, sudan(n, x, y-1), sudan(n, x, y-1) + y)
+    mem[key] = sudanM(n-1, sudanM(n, x, y-1), sudanM(n, x, y-1) + y)
     return mem[key]
 
-print(sudanM(1, 1, 3))
-print(sudan(1, 1, 3))
+# największe sensowne wywołanie sudan dla n=2 i n=3
+print("sudan:\n    " +
+str(sudan(2, 1, 2)) + 
+"\n    " +
+str(sudan(3, 1, 1)))
+
+# największe sensowne wywołanie sudanM dla n=2 i n=3
+print("sudanM:\n    " +
+str(sudanM(2, 5, 2)) + 
+"\n    " +
+str(sudanM(3, 1, 1)))
