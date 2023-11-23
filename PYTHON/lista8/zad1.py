e77f25b21ae01bc4180c37ebadfcc59b7aeafab4
Rafal <rafal@leja.org.pl>
2023-11-23 14:12:30 +0100

    dodanie komentarzy

--- PYTHON/lista8/zad1.py
+++ PYTHON/lista8/zad1.py
@@ -1,4 +1,6 @@
 # klucz API do accuweather należy umieścic w pliku api.key
+# Rafał Leja
+# 22.11.2023
 
 import asyncio
 import aiohttp
@@ -11,27 +13,33 @@ async def fetch(client):
     return await resp.text()
 
 async def fetchWTHR(client, keyFile, query):
-  f = open(keyFile, "r")
-  if f == False:
-    raise FileNotFoundError
-  key = f.read()
-  searchUrl = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete"
-  forecastUrl = "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/"
-  searchUrl += "?apikey=" + key + "&q=" + quote(query) + "&language=pl-pl"
-  async with client.get(searchUrl) as searchResp:
-    assert searchResp.status == 200
-    #  await searchResp.text()
-    searchData = json.loads(await searchResp.text())
-  cityKey = searchData[0]["Key"]
-  forecastUrl += cityKey + "?apikey=" + key + "&language=pl-pl&details=false&metric=true"
-  async with client.get(forecastUrl) as forecastResp:
-    assert forecastResp.status == 200
-    forecastData = json.loads(await forecastResp.text())
-  outDict = {"Name" : searchData[0]["LocalizedName"],
-             "DateTime" : forecastData[0]["DateTime"],
-             "Desc" : forecastData[0]["IconPhrase"],
-             "PP" : forecastData[0]["PrecipitationProbability"],
-             "Temp" : forecastData[0]["Temperature"]["Value"]}
+  # wyłapywanie błędu braku pliku
+  try:
+    f = open(keyFile, "r")
+    key = f.read()
+    searchUrl = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete"
+    forecastUrl = "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/"
+    searchUrl += "?apikey=" + key + "&q=" + quote(query) + "&language=pl-pl"
+    # Wysyłamy zapytanie o wyszukanie miasta o nazwie query
+    async with client.get(searchUrl) as searchResp:
+      assert searchResp.status == 200
+      searchData = json.loads(await searchResp.text())
+    # z odpowiedzi wyciągamy id miasta
+    cityKey = searchData[0]["Key"]
+    forecastUrl += cityKey + "?apikey=" + key + "&language=pl-pl&details=false&metric=true"
+    # wysyłamy zapytanie o prognoze dla danego miasta po id
+    async with client.get(forecastUrl) as forecastResp:
+      assert forecastResp.status == 200
+      forecastData = json.loads(await forecastResp.text())
+    # zwracamy słownik z odpowiedznimi danymi
+    outDict = {"Status" : "OK",
+              "Name" : searchData[0]["LocalizedName"],
+              "DateTime" : forecastData[0]["DateTime"],
+              "Desc" : forecastData[0]["IconPhrase"],
+              "PP" : forecastData[0]["PrecipitationProbability"],
+              "Temp" : forecastData[0]["Temperature"]["Value"]}
+  except:
+    outDict = {"Status" : "No key file"}
   return outDict
 
 async def main():
@@ -45,10 +53,13 @@ async def main():
   return 0
 
 def printWeather(data):
-  print(
-    f'''Temperatura teraz w mieście {data["Name"]} to {data["Temp"]},
-    {data["Desc"]}, prawdopodobieństwo opadów {data["PP"]}%'''
-  )
+  if data["Status"] == "OK":
+    print(
+      f'''Temperatura teraz w mieście {data["Name"]} to {data["Temp"]},
+      {data["Desc"]}, prawdopodobieństwo opadów {data["PP"]}%'''
+    )
+  else:
+    print("Brak klucza")
 
 def printLaunch(data):
   nextLaunch = data["results"][0]
@@ -56,5 +67,5 @@ def printLaunch(data):
   f'''Następny lot w kosmos odbędzie się {nextLaunch["window_start"]}, 
   z {nextLaunch["pad"]["location"]["name"]}. Rakieta nośna to {nextLaunch["rocket"]["configuration"]["full_name"]}.''')
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # Bez tej linijki program zgłasza RuntimeError
+# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # Bez tej linijki program zgłasza RuntimeError na Windowsie
 asyncio.run(main())
