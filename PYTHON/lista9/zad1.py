d45e07a2bb279acfa8d57e9e7784bc0b68bf6955
Rafal <rafal@leja.org.pl>
2023-11-28 14:04:42 +0100

    dodanie tytułów

--- PYTHON/lista9/zad1.py
+++ PYTHON/lista9/zad1.py
@@ -1,24 +1,35 @@
 import csv
 import matplotlib.pyplot as plt
 
-with open('inflacja.csv', newline='') as csvfile:
+with open('inflacja.csv', encoding="utf-8") as csvfile:
     dane = csv.DictReader(csvfile, delimiter=";")
     rok2021 = [[],[]]
     rok2022 = [[],[]]
     
     for rzad in dane:
+        print(rzad.keys())
         if rzad["Sposób prezentacji"] == "Poprzedni miesi¹c = 100":
             if rzad["Rok"] == "2022":
                 rok2022[0].insert(0, rzad["Miesi¹c"])
-                rok2022[1].insert(1, float(rzad["Wartoœæ"].replace(",",".")))
+                rok2022[1].insert(1, float(rzad["Wartość"].replace(",",".")))
             elif rzad["Rok"] == "2021":
                 rok2021[0].insert(0, rzad["Miesi¹c"])
-                rok2021[1].insert(1, float(rzad["Wartoœæ"].replace(",",".")))
+                rok2021[1].insert(1, float(rzad["Wartość"].replace(",",".")))
             elif int(rzad["Rok"]) < 2021:
                 break
     
     print(rok2021)
-    plt.plot(rok2021[0], rok2021[1])
-    plt.ylabel("Wzkaźnik inflacji względem poprzedniego miesiąca")
-    plt.xlabel("Miesiąc")
+    fig, axs = plt.subplots(2)
+    axs[0].plot(rok2021[0], rok2021[1])
+    axs[0].set_title('Inflacja w Polsce w 2021 r.')
+    axs[0].set(ylabel="Wzkaźnik inflacji względem poprzedniego miesiąca",
+               xlabel="Miesiąc")
+    axs[1].plot(rok2022[0], rok2022[1])
+    axs[1].set_title("Inflacja w Polsce w 2022 r.")
+    axs[1].set(ylabel="Wzkaźnik inflacji względem poprzedniego miesiąca",
+               xlabel="Miesiąc")
+    
+    for ax in fig.get_axes():
+        ax.label_outer()
+
     plt.show()
