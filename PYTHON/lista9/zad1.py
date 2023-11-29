ea365c9342747f4a42cda1763f44f078ca7481f3
RafalLeja <rafal@leja.org.pl>
2023-11-29 20:17:38 +0100

    naprawienie przewidywania

--- PYTHON/lista9/zad1.py
+++ PYTHON/lista9/zad1.py
@@ -8,18 +8,18 @@ import csv
 import matplotlib.pyplot as plt
 import json
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 
 def daneCSV(plik):
     with open(plik, encoding='utf-8') as csvfile:
         dane = csv.DictReader(csvfile, delimiter=";")
-        rok = "2022"
+        rok = "2023"
         daneInf = []
         daneRok = []
 
         for rzad in dane:
-            if rzad["Rok"] == "2023":
-                continue
-
             if rzad["Sposób prezentacji"] == "Analogiczny miesiąc poprzedniego roku = 100":
                 if rzad["Rok"] == rok:
                     daneRok.append(float(rzad["Wartość"].replace(",",".")))
@@ -54,13 +54,13 @@ def jsonAvg(dane):
 
 def flat(arr):
     out = []
-    for i in arr:
+    for i in arr[::-1]:
         for j in i:
             out.append(j)
     return out
 
 async def main():
-    daneInf = daneCSV('inflacja.csv')
+    daneInf = np.array(daneCSV('inflacja.csv'))
     async with aiohttp.ClientSession() as client:
         dolar2021 = await fetch(client, "https://api.nbp.pl/api/exchangerates/rates/a/usd/2021-01-01/2021-12-31/?format=json")
         dolar2022 = await fetch(client, "https://api.nbp.pl/api/exchangerates/rates/a/usd/2022-01-01/2022-12-31/?format=json")
@@ -70,10 +70,10 @@ async def main():
 
     mies = [i+1 for i in range(12)]
 
-    fig, (ax1a, ax2a, ax3) = plt.subplots(3)
+    fig, (ax1a, ax2a, ax3) = plt.subplots(1, 3)
 
     fig.suptitle("Inflacja w Polsce względem analogicznego miesiąca zeszłego roku")
-    l1 = ax1a.bar(mies, daneInf[1], width=1, color="blue", edgecolor="white", linewidth=0.7)
+    l1 = ax1a.bar(mies, daneInf[2], width=1, color="blue", edgecolor="white", linewidth=0.7)
     ax1a.set(ylim=(95,120),
              title='2021',
              ylabel="%",
@@ -85,7 +85,7 @@ async def main():
              ylim=(3.7,5))
     ax1a.legend((l1, l2), ('współczynnik inflacji', "kurs USD/PLN"), loc='upper left')
     
-    l3 = ax2a.bar(mies, daneInf[0], width=1, color="blue", edgecolor="white", linewidth=0.7)
+    l3 = ax2a.bar(mies, daneInf[1], width=1, color="blue", edgecolor="white", linewidth=0.7)
     ax2a.set(ylim=(95,120),
                title="2022",
                ylabel="%",
@@ -97,16 +97,23 @@ async def main():
              ylim=(3.7,5))
     ax2a.legend((l3, l4), ('współczynnik inflacji', "kurs USD/PLN"), loc='upper left')
 
-    daneFlat = flat(daneInf)
-    model = SARIMAX(daneFlat, order=(1, 1, 1), seasonal_order=(0, 0, 0, 0))
-    model_fit = model.fit(method="lbfgs")
-    future = model_fit.predict(1, 12)
-    print(future)
-    ax3.bar(mies, future, width=1, color="blue", edgecolor="white", linewidth=0.7)
+
+    daneFlat = flat(daneInf[1:])
+    model = SARIMAX(daneFlat, order=(1, 1, 2), seasonal_order=(0, 0, 2, 12),  enforce_stationarity=False, enforce_invertibility=False)
+    model_fit = model.fit(disp=False)
+    yhat = model_fit.predict(0, len(daneFlat), dynamic=False)
+    future = model_fit.get_forecast(steps=12).summary_frame(alpha=0.05)['mean']
+
+    l5 = ax3.bar(mies, future, width=1, color="green", edgecolor="white", linewidth=0.7)
     ax3.set(ylim=(95,120),
-               title="2023",
+               title="2023 - przewidywane",
                ylabel="%",
                xlabel="Miesiąc")
+    
+    ax3b = ax3.twinx()
+    l6, = ax3b.plot(mies[:10], daneInf[0][:10], color="black")
+
+    ax3.legend((l5, l6), ('dane przewidywanie', 'dane rzeczywiste'))
 
     for ax in fig.get_axes():
         ax.label_outer()
