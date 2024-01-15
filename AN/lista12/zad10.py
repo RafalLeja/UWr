2e533eea1ae040b17a31bd14e992cf9ecea0dd2d
RafalLeja <rafal@leja.org.pl>
2024-01-16 00:51:41 +0100

    szybsza wersja 10

--- AN/lista12/zad10.py
+++ AN/lista12/zad10.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def f(x):
-    return 2024*x**8-1977*x**4-1981
+    return (2024*x**8)-(1977*x**4)-1981
 
 def h(x):
     return 1/(1+x**2)
@@ -12,29 +12,44 @@ def g(x):
     return math.sin(5*x-math.pi/3)
 
 def i(x):
-    return x**2
+    arr = [1, 2, 2, 0, 1]
+    return arr[int(x*4)]
 
-def T(i, fn, a, b):
-    h = (b-a)/(2**i)
-    tn = [a + i*h for i in range(2**i+1)]
+# def T(i, fn, a, b):
+#     h = (b-a)/(2**i)
+#     tn = [a + i*h for i in range(2**i+1)]
+#     sum = 0
+#     for j in range(2**i):
+#         sum += (fn(tn[j])+fn(tn[j+1]))/2
+#     return h*sum
+
+def quickM(n, fn, a, b):
+    h = (b-a)/n
     sum = 0
-    for j in range(2**i):
-        sum += (fn(tn[j])+fn(tn[j+1]))/2
+    for i in range(1, n):
+        sum += fn(a+(2*i-1)*h*0.5)
     return h*sum
 
 def Romberg(fn, a, b, n):
     tablica = [[0 for i in range(n+1)] for j in range(n+1)]
-    for i in range(n+1):
-        tablica[0][i] = T(i, fn, a, b)
+
+    tablica[0][0] = (b-a)*(fn(a)+fn(b))/2
+
+    for i in range(1, n+1):
+        tablica[0][i] = 0.5*(tablica[0][i-1] + quickM(2**(i-1), fn, a, b))
 
     for col in range(1,n+1):
-        for row in range(n+1-col):
-            tablica[col][row+col] = 4**col * tablica[col-1][row+col] - tablica[col-1][row-1+col] / (4**col-1)
+        for row in range(col, n+1):
+            tablica[col][row] = (4**col * tablica[col-1][row] - tablica[col-1][row-1]) / (4**col-1)
     
-    return tablica
+    return tablica[-1][-1]
 
-tab1 = Romberg(h, -3, 3, 20)
-for x in range(len(tab1)):
-    for y in range(len(tab1[0])):
-        print(format(tab1[y][x], "e"), end=", ")
-    print("\n")
+# tab1 = Romberg(h, -3, 3, 20)
+# for x in range(len(tab1)):
+#     for y in range(len(tab1[0])):
+#         print(format(tab1[y][x], "f"), end=", ")
+#     print("\n")
+
+print(f"a) = {Romberg(f, -3, 2, 20)}")
+print(f"b) = {Romberg(h, -3, 3, 20)}")
+print(f"c) = {Romberg(g, -3*math.pi, math.pi/6, 20)}")
--- AN/lista12/zad6.py
+++ AN/lista12/zad6.py
@@ -1,6 +1,70 @@
-def Newton(fn, n, a, b):
-    tn = [a + (b - a) * i / n for i in range(n + 1)]
+import numpy as np
+import math
 
-    a = []
-    for i in range(n + 1):
-        a.append(fn(tn[i]))
+# def newton_cotes_coefficients(n):
+#     # prarametry dla int0_1
+#     x = np.linspace(0, 1, n)
+#     calki = np.zeros(n)
+#     for i in range(n):
+#         calki[i] = 1 / (i+1)
+#     Aix = np.zeros((n, n))
+#     for j in range(n):
+#         for i in range(n):
+#             Aix[j][i] = x[i]**j
+#     param = np.linalg.solve(Aix, calki)
+#     return param
+
+# def newton_cotes(fn, a, b, n):
+#     coefficients = newton_cotes_coefficients(n)
+#     arr = []
+#     for i in range(n):
+#         arr.append(((-1)**(2*(n-i)))/math.factorial(i))
+#     h = 1 / n
+#     result = 0
+#     for i in range(n):
+#         x = i*h
+#         result += coefficients[i] * fn(x*(b-a)+a)
+#     return (b-a)*result
+
+def newton_cotes_coefficients_fast(N):
+    # prarametry dla int0_1
+    xn = np.arange(N+1)
+    yi = xn / float(N)
+    ti = 2 * yi - 1
+    nvec = np.arange(N+1)
+    C = ti ** nvec[:, np.newaxis]
+    print(C)
+    Cinv = np.linalg.inv(C)
+    print(Cinv)
+    # improve precision of result
+    # for i in range(2):
+    #     Cinv = 2*Cinv - Cinv.dot(C).dot(Cinv)
+    vec = 2.0 / (nvec[::2]+1)
+    ai = Cinv[:, ::2].dot(vec) * (N / 2.)
+
+    return ai
+
+def newton_cotes(fn, a, b, n):
+    h = (b-a) / n
+    coefficients = []
+    for i in range(n):
+        licz = h*(-1)**(n-i) 
+        mian = math.factorial(i)*math.factorial(n-i)
+
+    for i in range(n):
+        arr.append(((-1)**(2*(n-i)))/math.factorial(i))
+    result = 0
+    for i in range(n):
+        x = i*h
+        result += coefficients[i] * fn(x*(b-a)+a)
+    return (b-a)*result
+
+def f(x):
+    return 1/(1+25*(x**2))
+
+def g(x):
+    return x
+
+for i in range(2, 3):
+    print(newton_cotes_coefficients_fast(i))
+    # print(f"n={i}, {newton_cotes(g, 0, 5, i)}")
