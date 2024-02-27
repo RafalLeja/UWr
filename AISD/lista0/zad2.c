e8c4c4b5c42f0e9fd47663226015abfd76de59dc
Rafal <rafal@leja.org.pl>
2024-02-27 10:33:45 +0100

    koniec zadania 2

--- AISD/lista0/zad2.c
+++ AISD/lista0/zad2.c
@@ -1,15 +1,19 @@
+// Rafał Leja
+// 340879
+// MBa
+
 #include <stdio.h>
 
 int main(void)
 {
-  int a;
-  int b;
+  int a, b;
   scanf("%d %d", &a, &b);
 
-  int counter = a + 2024 - a%2024;
+  int start = a + 2023 - (a-1)%2024;
 
-  printf("counter = %d\n", counter);
-
+  for (int i = start; i <= b; i+=2024) {
+    printf("%d\n", i);
+  }
 
   return 0;
 }
