81f8bca94deb0ff218f4b92642e1a8a5c32cb2ab
RafalLeja <rafal@leja.org.pl>
2024-03-04 11:47:00 +0100

    naprawione hashowanie i visited

--- SI/pracownia1/zad1.py
+++ SI/pracownia1/zad1.py
@@ -3,99 +3,112 @@ import sys
 from collections import deque
 
 class State:
-  def __init__(self, color, wKing, wTower, bKing, parents = []):
+  def __init__(self, color, wKing, wTower, bKing, parent = None, depth = 0):
     self.color = color
     self.wKing = wKing
     self.wTower = wTower
     self.bKing = bKing
-    self.parents = parents
-    self.neighbours = []
+    self.parent = parent
+    self.depth = depth
 
-  def generateNeighbours(self):
-    if self.color == "white":
-      for x in [-1, 0, 1]:
-        for y in [-1, 0, 1]:
-          kX = self.wKing[0] + x
-          kY = self.wKing[1] + y
-          # pozycja nie zmieniona
-          if kX == self.wKing[0] and kY == self.wKing[1]:
-            continue
-          # ruch króla poza planszę
-          if kX < 1 or kX > 8 or kY < 1 or kY > 8:
-            continue
-          # ruch króla na pole wieży
-          if kX == self.wTower[0] and kY == self.wTower[1]:
-            continue
-          # ruch króla na pole bite przez czarnego króla
-          if abs(kX - self.bKing[0]) < 2 and abs(kY - self.bKing[1]) < 2:
-            continue
-
-          self.neighbours.append(State("black", [kX, kY], self.wTower, self.bKing, self.parents + [self]))
-
-      for tX in range(1, 9):
-        # pozycja nie zmieniona
-        if tX == self.wTower[0]: 
-          continue   
-        # ruch wieży na pole białego króla
-        if tX == self.wKing[0] and self.wTower[1] == self.wKing[1]:
-          continue
-        # ruch wieży na pole bite przez czarnego króla
-        if abs(tX - self.bKing[0]) < 2 and abs(self.wTower[1] - self.bKing[1]) < 2:
-          continue
-        self.neighbours.append(State("black", self.wKing, [tX, self.wTower[1]], self.bKing, self.parents + [self]))
-
-      for tY in range(1, 9):
-        # pozycja nie zmieniona
-        if tY == self.wTower[1]: 
-          continue
-        # ruch wieży na pole białego króla
-        if tY == self.wKing[1] and self.wTower[0] == self.wKing[0]:
-          continue
-        # ruch wieży na pole bite przez czarnego króla
-        if abs(tY - self.bKing[1]) < 2 and abs(self.wTower[0] - self.bKing[0]) < 2:
-          continue
-
-        self.neighbours.append(State("black", self.wKing, [self.wTower[0], tY], self.bKing, self.parents + [self]))
-
-    else:
-      for x in [-1, 0, 1]:
-        for y in [-1, 0, 1]:
-          kX = self.bKing[0] + x
-          kY = self.bKing[1] + y
-          # pozycja się nie zmienia
-          if kX == self.bKing[0] and kY == self.bKing[1]:
-            continue
-          # ruch króla poza planszę
-          if kX < 1 or kX > 8 or kY < 1 or kY > 8:
-            continue
-          # ruch króla na pole bite przez wieżę
-          if kX == self.wTower[0] or kY == self.wTower[1]:
-            continue
-          # ruch króla na pole bite przez białego króla
-          if abs(kX - self.wKing[0]) < 2 and abs(kY - self.wKing[1]) < 2:
-            continue
-
-          self.neighbours.append(State("white", self.wKing, self.wTower, [kX, kY], self.parents + [self]))
-    
   def __str__(self):
     return f"{self.color} {chr(self.wKing[0]+96)}{self.wKing[1]} "\
           +f"{chr(self.wTower[0]+96)}{self.wTower[1]} "\
           +f"{chr(self.bKing[0]+96)}{self.bKing[1]}"
   
-  def __hash__(self) -> int:
-    return hash(str(self))
     
-  def checkMate(self):
-    if self.color == "black":
-      check = False
-      # czy król jest bity przez króla?
-      if abs(self.wKing[0] - self.bKing[0]) < 2 and abs(self.wKing[1] - self.bKing[1]) < 2:
-        check = True
-      # czy król jest bity przez wieżę?
-      if self.bKing[0] == self.wTower[0] or self.bKing[1] == self.wTower[1]:
-        check = True
-      if check == True and len(self.neighbours) == 0:
+def generateNeighbours(state, visited):
+  nextMoves = []
+  
+  if state.color == "white":
+    for (x, y) in [(-1, -1), (0, -1),  (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]:
+      kX = state.wKing[0] + x
+      kY = state.wKing[1] + y
+      # ruch króla poza planszę
+      if kX < 1 or kX > 8 or kY < 1 or kY > 8:
+        continue
+      # ruch króla na pole wieży
+      if kX == state.wTower[0] and kY == state.wTower[1]:
+        continue
+      # ruch króla na pole bite przez czarnego króla
+      if abs(kX - state.bKing[0]) < 2 and abs(kY - state.bKing[1]) < 2:
+        continue
+
+      nm = State("black", (kX, kY), state.wTower, state.bKing, state, state.depth+1)
+      pos = (nm.color, nm.wKing, nm.wTower, nm.bKing)
+      if pos not in visited:
+        nextMoves.append(nm)
+        visited.add(pos)
+
+    for tX in range(1, 9):
+      # pozycja nie zmieniona
+      if tX == state.wTower[0]: 
+        continue   
+      # ruch wieży na pole białego króla
+      if tX == state.wKing[0] and state.wTower[1] == state.wKing[1]:
+        continue
+      # ruch wieży na pole bite przez czarnego króla
+      if abs(tX - state.bKing[0]) < 2 and abs(state.wTower[1] - state.bKing[1]) < 2:
+        continue
+      nm = State("black", state.wKing, (tX, state.wTower[1]), state.bKing, state, state.depth+1)
+      pos = (nm.color, nm.wKing, nm.wTower, nm.bKing)
+      if pos not in visited:
+        nextMoves.append(nm)
+        visited.add(pos)
+
+    for tY in range(1, 9):
+      # pozycja nie zmieniona
+      if tY == state.wTower[1]: 
+        continue
+      # ruch wieży na pole białego króla
+      if tY == state.wKing[1] and state.wTower[0] == state.wKing[0]:
+        continue
+      # ruch wieży na pole bite przez czarnego króla
+      if abs(tY - state.bKing[1]) < 2 and abs(state.wTower[0] - state.bKing[0]) < 2:
+        continue
+
+      nm = State("black", state.wKing, (state.wTower[0], tY), state.bKing, state, state.depth+1)
+      pos = (nm.color, nm.wKing, nm.wTower, nm.bKing)
+      if pos not in visited:
+        nextMoves.append(nm)
+        visited.add(pos)
+
+  else:
+    for (x, y) in [(-1, -1), (0, -1),  (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]:
+      kX = state.bKing[0] + x
+      kY = state.bKing[1] + y
+      # ruch króla poza planszę
+      if kX < 1 or kX > 8 or kY < 1 or kY > 8:
+        continue
+      # ruch króla na pole bite przez wieżę
+      if kX == state.wTower[0] or kY == state.wTower[1]:
+        continue
+      # ruch króla na pole bite przez białego króla
+      if abs(kX - state.wKing[0]) < 2 and abs(kY - state.wKing[1]) < 2:
+        continue
+
+      nm = State("white", state.wKing, state.wTower, (kX, kY), state, state.depth+1)
+      pos = (nm.color, nm.wKing, nm.wTower, nm.bKing)
+      if pos not in visited:
+        nextMoves.append(nm)
+        visited.add(pos)
+
+  return nextMoves
+  
+def checkMate(state : State):
+    # jedyne możliwe maty to gdy król jest na krawędzi planszy i jest atakowany przez wieżę
+    if state.bKing[0] == 1 and state.wKing[0] == 3 and state.bKing[1] == state.wKing[1] and state.wTower[0] == 1 and abs(state.bKing[1] - state.wTower[1]) > 1:
         return True
+
+    if state.bKing[0] == 8 and state.wKing[0] == 6 and state.bKing[1] == state.wKing[1] and state.wTower[0] == 8 and abs(state.bKing[1] - state.wTower[1]) > 1:
+        return True
+
+    if state.bKing[1] == 1 and state.wKing[1] == 3 and state.bKing[0] == state.wKing[0] and state.wTower[1] == 1 and abs(state.bKing[0] - state.wTower[0]) > 1:
+        return True
+
+    if state.bKing[1] == 8 and state.wKing[1] == 6 and state.bKing[0] == state.wKing[0] and state.wTower[1] == 8 and abs(state.bKing[0] - state.wTower[0]) > 1:
+        return True
+    
     return False
 
 def main():
@@ -116,28 +129,31 @@ def main():
   return
 
 def solve( color, wKing, wTower, bKing, mode):
-  start = State(color, [ord(wKing[0])-96, int(wKing[1])], 
-                       [ord(wTower[0])-96, int(wTower[1])], 
-                       [ord(bKing[0])-96, int(bKing[1])])
+  start = State(color, (ord(wKing[0])-96, int(wKing[1])), 
+                       (ord(wTower[0])-96, int(wTower[1])), 
+                       (ord(bKing[0])-96, int(bKing[1])))
   queue = deque([start])
   visited = set([])
+  total = 0
   while len(queue) > 0:
+    total += 1
     current = queue.popleft()
 
-    if current in visited:
-      continue
-    visited.add(current)
-    
-    current.generateNeighbours()
-    queue.extend(current.neighbours)
-
-    if current.checkMate():
+    if checkMate(current):
+      moves = 0
       if mode == "debug": 
         print("Checkmate")
-        for p in current.parents:
-          print(str(p))
-        print(str(current))
-      return len(current.parents)
+      
+      while current.parent != None:
+        if mode == "debug":
+          print(str(current))
+        current = current.parent
+        moves += 1
+
+      return moves
+    
+    queue.extend(generateNeighbours(current, visited))
+
 
   return math.inf
 
