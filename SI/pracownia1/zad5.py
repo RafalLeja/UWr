0074d295ab9527517c8e6a8d541aa1f56170af27
Rafal <rafal@leja.org.pl>
2024-03-05 23:34:39 +0100

    zad5 dodanie debugu i pierwszych rozwiązań

--- SI/pracownia1/zad5.py
+++ SI/pracownia1/zad5.py
@@ -1,12 +1,48 @@
-import opt_dist from zad4
+import random
 
-def solved(board, rows, cols):
-  for r in rows:
-    if opt_dist(board[r], r) != 0:
-      return False
-  for c in cols:
-    if opt_dist(board[c], c) != 0:
-      return False
+DEBUG = True
+
+def blockCount(L):
+  s = 0
+  for i in L:
+    if i == '#':
+      s += 1
+  return s
+  
+def failedRows(board, rows):
+  fRows = []
+  for r in range(len(rows)):
+    if blockCount(board[r]) != rows[r]:
+      fRows.append(r)
+  return fRows
+
+def failedCols(board, cols):
+  fCols = []
+  for c in range(len(cols)):
+    col = []
+    for r in range(len(board)):
+      col.append(board[r][c])
+    if blockCount(col) != cols[c]:
+      fCols.append(c)
+  return fCols
+
+def fillFulls(board, rows, cols, rowsLeft, colsLeft):
+  for r in range(len(rows)):
+    if rows[r] == len(rows):
+      for c in range(len(cols)):
+        board[r][c] = '#'
+      rowsLeft[r] = 0
+      for i in range(len(cols)):
+        colsLeft[i] -= 1
+  
+  for c in range(len(cols)):
+    if cols[c] == len(cols):
+      for r in range(len(rows)):
+        board[r][c] = '#'
+      colsLeft[c] = 0
+      for i in range(len(rows)):
+        rowsLeft[i] -= 1
+  return
 
 def main():
   input_file = open('zad5_input.txt', 'r')
@@ -17,7 +53,11 @@ def main():
   y = int(y)
   rows = []
   cols = []
-  board = [[0 for i in range(x)] for j in range(y)]
+  rowsLeft = []
+  colsLeft = []
+  board = [['.' for i in range(x)] for j in range(y)]
+  fRows = []
+  fCols = []
 
   for line in range(x):
     rows.append(int(input_file.readline().strip()))
@@ -25,9 +65,46 @@ def main():
   for line in range(y):
     cols.append(int(input_file.readline().strip()))
 
-  while solved(board, rows, cols) == False:
-    print("not solved")
-  print(x, y, rows, cols)
+  input_file.close()
+
+  rowsLeft = rows.copy()
+  colsLeft = cols.copy()
+  fillFulls(board, rows, cols, rowsLeft, colsLeft)
+  fRows = failedRows(board, rows)
+  fCols = failedCols(board, cols)
+  while len(fRows) > 0  and len(fCols) > 0:
+    r = max(fRows, key = lambda x: rowsLeft[x])
+    c = random.choice(fCols)
+    if board[r][c] == '#':
+      continue
+    board[r][c] = '#'
+    rowsLeft[r] -= 1
+    colsLeft[c] -= 1
+    fRows = failedRows(board, rows)
+    fCols = failedCols(board, cols)
+    if DEBUG:
+      for i in range(len(rows)):
+        print(rows[i], rowsLeft[i], board[i])
+      print("      ", end = '')
+
+      for i in range(len(cols)):
+        print(colsLeft[i], "   ", end = '')
+      print()
+
+      print("      ", end = '')
+      for i in range(len(cols)):
+        print(cols[i], "   ", end = '')
+        
+      print()
+      print(fRows)
+      print(fCols)
+      input()
+    
+  for i in range(len(rows)):
+    for j in range(len(cols)):
+      output_file.write(board[i][j])
+    output_file.write('\n')
+  output_file.close()
   return
 
 if __name__ == "__main__":
