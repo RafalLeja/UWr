11044bc8950f9ee4bd5572f2392d5d040287241e
Rafal <rafal@leja.org.pl>
2024-03-05 17:45:05 +0100

    dodanie opt

--- SI/pracownia1/zad5.py
+++ SI/pracownia1/zad5.py
@@ -1,3 +1,13 @@
+import opt_dist from zad4
+
+def solved(board, rows, cols):
+  for r in rows:
+    if opt_dist(board[r], r) != 0:
+      return False
+  for c in cols:
+    if opt_dist(board[c], c) != 0:
+      return False
+
 def main():
   input_file = open('zad5_input.txt', 'r')
   output_file = open('zad5_output.txt', 'w')
@@ -15,6 +25,8 @@ def main():
   for line in range(y):
     cols.append(int(input_file.readline().strip()))
 
+  while solved(board, rows, cols) == False:
+    print("not solved")
   print(x, y, rows, cols)
   return
 
