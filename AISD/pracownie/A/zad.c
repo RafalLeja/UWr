6319c67d5c5b78e38d0d4a32040b0fcccdd0ebf9
RafalLeja <rafal@leja.org.pl>
2024-03-06 14:30:36 +0100

    rozwiązane, TLE przy 4,5

--- AISD/pracownie/A/zad.c
+++ AISD/pracownie/A/zad.c
@@ -3,56 +3,46 @@
 // MBa
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void buildHeap(int n, unsigned short *Heap, unsigned short *Hierarchy);
-
-void moveDown(int i, int n, unsigned short *Heap, unsigned short *Hierarchy);
+int isParent(int a, int b, unsigned short *hierarchy);
 
 int main(void)
 { 
   unsigned short n, q;
   scanf("%hu %hu", &n, &q);
-  unsigned short desc[n-1];
+
+  unsigned short *hierarchy;
+  hierarchy = (unsigned short *)calloc((n-1), sizeof(unsigned short));
+
   for (int i = 0; i < n-1; i++) {
-    scanf("%hu", &desc[i]);
+    scanf("%hu", &hierarchy[i]);
   }
 
-  unsigned short heap[n];
-  for (int i = 0; i < n; i++) {
-    heap[i] = i+1;
-  }
- 
-  buildHeap(n, heap, desc);
-
-  // printf("n = %hu, q = %hu\n", n, q);
-  for (int i = 0; i < n; i++) {
-    printf("K[%d] = %hu\n", i, heap[i]);
+  for (int i = 0; i < q; i++) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (isParent(a, b, hierarchy)) {
+      printf("TAK\n");
+    } else {
+      printf("NIE\n");
+    }
   }
 
-  // for (int i = 0; i < n-1; i++) {
-    // printf("desc[%d] = %hu\n", i, desc[i]);
-  // }
-
-  // printf("size = %ld", sizeof(desc));
-
+  free(hierarchy);
   return 0;
 }
 
-void buildHeap(int n, unsigned short *Heap, unsigned short *Hierarchy){
-  for (int i = n/2 + n%2; i > 0; i--) {
-    printf("i = %d\n", i);
-    moveDown(i, n , Heap, Hierarchy);
+int isParent(int a, int b, unsigned short *hierarchy){
+  if (a == 1) return 1;
+  while (b != 1 && b != a) {
+    b = hierarchy[b-2];
   }
+  return b == a;
 }
 
-void moveDown(int i, int n, unsigned short *Heap, unsigned short *Hierarchy){
-  int j = 0;
-  while (i!=j) {
-    j = i;
-    if (2*j <= n && Hierarchy[2*j] > Hierarchy[i]) i = 2*j;
-    if (2*j < n && Hierarchy[2*j+1] > Hierarchy[i]) i = 2*j+1;
-    unsigned short temp = Heap[j];
-    Heap[j] = Heap[i];
-    Heap[i] = temp;
-  }
-}
+// int isParent(int a, int b, unsigned short *hierarchy){
+//   if (a == 1 || a == b) return 1;
+//   if (b == 1) return 0;
+//   return isParent(a, hierarchy[b-2], hierarchy);
+// }
