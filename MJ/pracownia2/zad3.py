762298e89164a3afe5c4558f06e40ae95f15e528
RafalLeja <rafal@leja.org.pl>
2024-10-28 12:18:45 +0100

    zad3

--- MJ/pracownia2/zad3.py
+++ MJ/pracownia2/zad3.py
@@ -1,56 +1,63 @@
-from transformers import AutoModelForMaskedLM, AutoTokenizer
 import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from torch.nn import functional as F
+import random
 
-# Wczytaj model językowy Polka (eryk-mazus/polka)
-tokenizer = AutoTokenizer.from_pretrained("eryk-mazus/polka-1.1b")
-model = AutoModelForMaskedLM.from_pretrained("eryk-mazus/polka-1.1b")
+model_name = 'flax-community/papuGaPT2'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+
+def log_probs_from_logits(logits, labels):
+    logp = F.log_softmax(logits, dim=-1)
+    logp_label = torch.gather(logp, 2, labels.unsqueeze(2)).squeeze(-1)
+    return logp_label
+    
+            
+def sentence_prob(sentence_txt):
+    input_ids = tokenizer(sentence_txt, return_tensors='pt')['input_ids'].to(device)
+    length = input_ids.size(1)
+    with torch.no_grad():
+        output = model(input_ids=input_ids)
+        log_probs = log_probs_from_logits(output.logits[:, :-1, :], input_ids[:, 1:])
+        seq_log_probs = torch.sum(log_probs)
+    return seq_log_probs.cpu().numpy().item()
+
+options = "wprost|wyprosty|wyprostu|wyprost uwielbiała|wielbił|wielbiła|uwielbił|wielbiło|uwielbiał|uwielbiało|uwielbiały słuchać|osłuchać|słychać|usłuchać o|i|e|a|ó|ę|y|ą|u wartościach własnych|owłosionych macierzy|mocarz|macierzą|macierze|mocarza|mocarze|mocarzy|macierz"
+options = [x.split('|') for x in options.split(' ')]
+
+BEAM_SIZE = 3
+
+beams = [(x, 0) for x in options[0]][0:BEAM_SIZE]
+for depth in range(1, len(options)):
+    variations = options[depth]
+    print("depth", depth)
+    print(beams)
+    new_beams = []
+    for i, beam in enumerate(beams):
+        print(f'Processing beam {i+1}/{len(beams)}')
+        best = []
+        for variation in variations:
+            sentence = beam[0] + ' ' + variation
+            prob = sentence_prob(sentence)
+            best.append((sentence, prob))
+        best = sorted(best, key=lambda x: x[1], reverse=True)
+        best = best[:BEAM_SIZE]
+        new_beams.extend(best)
+    beams = new_beams
+
+beams = sorted(beams, key=lambda x: x[1], reverse=True)
+correct_sentence = " ".join([o[0] for o in options])
+
+[print(b) for b in beams]
+beams = [b[0].split(' ') for b in beams]
+print(correct_sentence)
 
 
-def generate_sentences(variants):
-    """
-    Generuje wszystkie możliwe zdania na podstawie listy wariantów słów.
-    """
-    if len(variants) == 0:
-        return ['']
-
-    first, *rest = variants
-    rest_sentences = generate_sentences(rest)
-    sentences = []
-    for word in first:
-        for sentence in rest_sentences:
-            sentences.append(f"{word} {sentence}".strip())
-    return sentences
-
-
-def get_best_sentence(sentences, beam_width=3):
-    """
-    Zwraca najlepsze zdanie na podstawie modelu językowego z użyciem beam search.
-    """
-    sentence_scores = []
-    for sentence in sentences:
-        inputs = tokenizer(sentence, return_tensors="pt")
-        with torch.no_grad():
-            outputs = model(**inputs).logits
-            # Oblicz log-probability jako ocenę zdania
-            log_prob = torch.sum(outputs)
-            sentence_scores.append((sentence, log_prob.item()))
-
-    # Sortowanie zdań po log-probability i wybór najlepszych wyników
-    sorted_sentences = sorted(
-        sentence_scores, key=lambda x: x[1], reverse=True)
-    return [sentence for sentence, _ in sorted_sentences[:beam_width]]
-
-
-# Przykładowe dane wejściowe
-text = "wprost|wyprosty|wyprostu|wyprost uwielbiała|wielbił|wielbiła|uwielbił|wielbiło|uwielbiał|uwielbiało|uwielbiały słuchać|osłuchać|słychać|usłuchać o|i|e|a|ó|ę|y|ą|u wartościach własnych|owłosionych macierzy|mocarz|macierzą|macierze|mocarza|mocarze|mocarzy|macierz"
-
-# Przetwórz tekst do listy wariantów słów
-variants = [item.split('|') for item in text.split()]
-
-# Wygeneruj wszystkie możliwe zdania
-possible_sentences = generate_sentences(variants)
-
-# Znajdź najlepsze zdanie za pomocą beam search
-best_sentences = get_best_sentence(possible_sentences)
-
-print("Najlepsze zdanie:", best_sentences[0])
+index = [i for i, beam in enumerate(beams) if beam[0] == correct_sentence]
+if len(index) == 0:
+    print("Nie znaleziono poprawnego zdania")
+else:
+    print(index)
+    
