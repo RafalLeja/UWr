689eb694a57c305f2fd1c5916558b1cf2f4061da
rafal <rafal@leja.org.pl>
2025-10-27 16:15:49 +0100

    KRYPTO: L3: 12+

--- KRYPTO/lista3/zad12.py
+++ KRYPTO/lista3/zad12.py
@@ -1,6 +1,18 @@
-import pyUnicodeSteganography as usteg
+encoded_text = open("./zad12.txt", "r").read()
 
-encoded_text = open("./zad12.txt", "rb").read()
-print(encoded_text[:5])
-decodec_text = usteg.decode(encoded_text, method="lookalike", binary=True)
-print(decodec_text)
+ones = "BZINoOPTaBeoOpcTy"
+zeros = "ΒΖΙΝοΟΡΤаВеоОрсТу"
+twos = ""
+
+blacklist = "!(),.:;ąćęłńóśŚżŻ"
+
+decoded_text = ""
+for i in list(encoded_text):
+    if i in blacklist:
+        continue
+    elif i in zeros:
+        decoded_text += "1"
+    elif i in ones:
+        decoded_text += "0"
+
+print(decoded_text)
