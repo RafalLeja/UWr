4dc581c13a32a9eb1cea1b836db3f11a65dd9e6c
rafal <rafal@leja.org.pl>
2025-10-28 11:48:10 +0100

    SW: L3: 3+

--- SW/lista3/zad3/exp.py
+++ SW/lista3/zad3/exp.py
@@ -1,9 +1,8 @@
-import math
-
 n = 1024
 
-with open("table.c", "w") as outfile:
-    outfile.write("uint8_t exp[] = {\n")
+with open("table.h", "w") as outfile:
+    outfile.write("#include <avr/pgmspace.h>\n")
+    outfile.write("static const uint16_t exp_table[] PROGMEM = {\n")
     for i in range(n):
         outfile.write(f"{int(((i / n) ** 2.2) * 1024)},\n")
-    outfile.write("}")
+    outfile.write("};")
