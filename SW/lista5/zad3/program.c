3b00c1b33a1c7aefda2776dbc2116d2744da6c71
rafal <rafal@leja.org.pl>
2025-11-11 16:51:45 +0100

    SW: L5: 2+

--- SW/lista5/zad3/program.c
+++ SW/lista5/zad3/program.c
@@ -1,8 +1,11 @@
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
 #include <avr/sleep.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <util/delay.h>
 
 #define BAUD 9600                              // baudrate
 #define UBRR_VALUE ((F_CPU) / 16 / (BAUD) - 1) // zgodnie ze wzorem
@@ -11,7 +14,9 @@
 #define LED_DDR DDRB
 #define LED_PORT PORTB
 
-#define AVG_LEN 16
+#define AVG_LEN 64
+
+uint16_t data_buffer[AVG_LEN];
 
 // inicjalizacja UART
 void uart_init() {
@@ -40,6 +45,11 @@ int uart_receive(FILE *stream) {
   return UDR0;
 }
 
+void uart_wait() {
+  while (!(UCSR0A & _BV(TXC0)))
+    ;
+}
+
 // inicjalizacja ADC
 void adc_init() {
   ADMUX = _BV(REFS0); // referencja AVcc
@@ -48,7 +58,21 @@ void adc_init() {
   DIDR0 = _BV(ADC0D); // wyłącz wejście cyfrowe na ADC0
   // częstotliwość zegara ADC 125 kHz (16 MHz / 128)
   ADCSRA = _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2); // preskaler 128
-  ADCSRA |= _BV(ADEN);                           // włącz ADC
+  ADCSRA |= _BV(ADEN) | _BV(ADIE);               // włącz ADC i interrupt
+}
+
+ISR(ADC_vect) {
+  // przerwanie tylko wybudza CPU ze snu
+}
+
+uint16_t variance(uint16_t avg) {
+  uint16_t var = 0;
+  for (uint16_t i = 0; i < AVG_LEN; i++) {
+    int16_t diff = data_buffer[i] - avg;
+    // printf("diff: %" PRId16 "\r\n", diff);
+    var += diff * diff;
+  }
+  return var / AVG_LEN;
 }
 
 FILE uart_file;
@@ -63,24 +87,48 @@ int main() {
   adc_init();
   // mierz napięcie
 
-  uint16_t prev = 0;
-  uint16_t var = 0;
+  uint16_t avg = 0;
   uint16_t i = 0;
+  printf("Wariancja:\t|\taktywne czekanie\t|\tnoise reduction\t|\r\n");
   while (1) {
-    for (i = 0; i < 4096; i++) {
+    avg = 0;
+    for (i = 0; i < AVG_LEN; i++) {
       ADCSRA |= _BV(ADSC); // wykonaj konwersję
       while (!(ADCSRA & _BV(ADIF)))
         ;                  // czekaj na wynik
       ADCSRA |= _BV(ADIF); // wyczyść bit ADIF (pisząc 1!)
       uint16_t v = ADC;    // weź zmierzoną wartość (0..1023)
-      // float vcc = (float)1126 / (float)v;
-      var += ((v > prev) * (v - prev)) + ((v <= prev) * (prev - v));
-      prev = v;
+      data_buffer[i] = v;
+      avg += v;
     }
-    // printf("Odczytano: %.3fV, Srednia z %d: %.3f\r\n", vcc, AVG_LEN, prev);
-    printf("Wariancja (aktywne czekanie): \t\t%d\r\n", var);
+    avg /= AVG_LEN;
 
+    // printf("srednia: %" PRIu16, avg);
+    // uart_wait();
+    // _delay_ms(100);
+
+    uint16_t var1 = variance(avg);
+
+    avg = 0;
+
+    sei();
     set_sleep_mode(SLEEP_MODE_ADC);
-    var = 0;
+    for (i = 0; i < AVG_LEN; i++) {
+      ADCSRA |= _BV(ADSC); // wykonaj konwersję
+      sleep_mode();        // uśpij CPU do zakończenia konwersji
+      uint16_t v = ADC;    // weź zmierzoną wartość (0..1023)
+      data_buffer[i] = v;
+      avg += v;
+    }
+    avg /= AVG_LEN;
+    _delay_ms(100);
+
+    // printf("srednia: %" PRIu16, avg);
+    // uart_wait();
+    // _delay_ms(100);
+
+    printf("\t\t\t%" PRIu16 "\t\t\t\t%" PRIu16 "\r\n", var1, variance(avg));
+    cli();
+    _delay_ms(2000);
   }
 }
