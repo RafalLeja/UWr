4b636d050faf0b9c19906b176bed3fc6c5e9f280
rafal <rafal@leja.org.pl>
2025-12-08 20:24:38 +0100

    ADD zad2 dziala

--- SW/lista8/zad2/main.c
+++ SW/lista8/zad2/main.c
@@ -1,12 +1,12 @@
 #include "FreeRTOS.h"
 #include "portmacro.h"
+#include "projdefs.h"
 #include "task.h"
 
 #include <avr/io.h>
 
 #include "queue.h"
 #include "uart.h"
-#include <avr/sfr_defs.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -14,27 +14,47 @@
 
 #define mainSERIAL_TASK_PRIORITY 1
 
+xQueueHandle queue;
+
 static void vBlinkLed(void *pvParameters);
 
 static void vSerial(void *pvParameters);
 
-static QueueHandle_t queue = NULL;
+static int uart_receive(FILE *stream) {
+  while (!(UCSR0A & _BV(RXC0)))
+    taskYIELD();
+  return UDR0;
+}
+
+static uint16_t getint() {
+  uint16_t num = 0;
+  // uint8_t len = 0;
+  while (1) {
+    char c = uart_receive(stdin);
+    putc(c, stdout);
+    if (c >= '0' && c <= '9') {
+      num *= 10;
+      num += c - '0';
+      // len += 1;
+    } else {
+      // putc('\r', stdout);
+      break;
+    }
+  }
+  return num;
+}
 
 int main(void) {
-  // Q
-  queue = xQueueCreate(1, 2);
-
-  // Create task.
   xTaskHandle blink_handle;
   xTaskHandle serial_handle;
 
+  queue = xQueueCreate(5, sizeof(uint16_t));
   xTaskCreate(vBlinkLed, "blink", configMINIMAL_STACK_SIZE, NULL,
               mainLED_TASK_PRIORITY, &blink_handle);
 
   xTaskCreate(vSerial, "serial", configMINIMAL_STACK_SIZE, NULL,
               mainSERIAL_TASK_PRIORITY, &serial_handle);
 
-  // Start scheduler.
   vTaskStartScheduler();
 
   return 0;
@@ -45,15 +65,17 @@ void vApplicationIdleHook(void) {}
 static void vBlinkLed(void *pvParameters) {
   DDRB |= _BV(PB5);
 
-  uint16_t time = 500;
+  TickType_t waitDelay = 500;
+  uint16_t t = 500;
   for (;;) {
-    if (uxQueueMessagesWaiting(queue)) {
-      xQueueReceive(queue, &time, 0);
+    if (uxQueueMessagesWaiting(queue) > 0) {
+      if (xQueueReceive(queue, &t, waitDelay) != pdTRUE)
+        continue;
     }
-    PORTB |= _BV(PB5);
-    vTaskDelay(time / portTICK_PERIOD_MS);
-    PORTB &= ~_BV(PB5);
-    vTaskDelay(50 / portTICK_PERIOD_MS);
+    PORTB ^= _BV(PB5);
+    vTaskDelay(t / portTICK_PERIOD_MS);
+    PORTB ^= _BV(PB5);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
@@ -64,9 +86,9 @@ static void vSerial(void *pvParameters) {
   uint16_t input;
 
   for (;;) {
-    printf("Podaj ms: ");
-    scanf("%d", &input);
-    printf("Czas: %d ms\r\n", input);
-    xQueueSend(queue, (void *)input, 0);
+    puts("Podaj czas\r\n");
+    input = getint();
+    xQueueSend(queue, &input, 1);
+    // printf("%d\r\n", input);
   }
 }
