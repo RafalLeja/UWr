fe7daf3ffe5cce8ba1172216ace23b4d2ae4b946
rafal <rafal@leja.org.pl>
2026-01-18 21:18:31 +0100

    ADD password mode in cli

--- CUDA/projekt/md5/src/cli.cu
+++ CUDA/projekt/md5/src/cli.cu
@@ -6,9 +6,7 @@
 struct args_t parse_args(int argc, char *argv[]) {
   int opt;
 
-  struct args_t args = {
-      NULL, stdout, false, 'h', 10,
-  };
+  struct args_t args = {NULL, stdout, false, 'h', 10, 8, 0b1111};
 
   static struct option long_options[] = {
       {"help", no_argument, 0, 'h'},
@@ -16,13 +14,20 @@ struct args_t parse_args(int argc, char *argv[]) {
       {"benchmark", no_argument, 0, 'b'},
       {"iterations", required_argument, 0, 'r'},
 
-      {"sum", no_argument, 0, 's'},
+      {"checksum", no_argument, 0, 'c'},
       {"input", required_argument, 0, 'i'},
       {"output", required_argument, 0, 'o'},
 
+      {"password", no_argument, 0, 'p'},
+      {"pass_length", required_argument, 0, 'm'},
+      {"numbers", no_argument, 0, 'n'},
+      {"lowercase", no_argument, 0, 'l'},
+      {"uppercase", no_argument, 0, 'u'},
+      {"special", no_argument, 0, 's'},
+
       {0, 0, 0, 0}};
 
-  while ((opt = getopt_long(argc, argv, "hbr:si:o:", long_options,
+  while ((opt = getopt_long(argc, argv, "hbr:ci:o:pm:nlus", long_options,
                             NULL)) != -1) {
     switch (opt) {
     case 'h':
@@ -37,9 +42,9 @@ struct args_t parse_args(int argc, char *argv[]) {
       args.iterations = atoi(optarg);
       break;
 
-    case 's':
-      args.mode = 's';
-      printf("[Sum]\n\n");
+    case 'c':
+      args.mode = 'c';
+      printf("[Checksum]\n\n");
       break;
     case 'i':
       args.input_file = fopen(optarg, "rb");
@@ -55,6 +60,31 @@ struct args_t parse_args(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
       }
       break;
+
+    case 'p':
+      args.mode = 'p';
+      printf("[Password Cracker]\n\n");
+      break;
+
+    case 'm':
+      args.pass_length = atoi(optarg);
+      break;
+
+    case 'n':
+      args.pass_type &= 0x1;
+      break;
+
+    case 'l':
+      args.pass_type &= 0x2;
+      break;
+
+    case 'u':
+      args.pass_type &= 0x4;
+      break;
+
+    case 's':
+      args.pass_type &= 0x8;
+      break;
     default:
       print_help(argv[0]);
     }
@@ -64,22 +94,32 @@ struct args_t parse_args(int argc, char *argv[]) {
 }
 
 void print_help(const char *program_name) {
-  printf("Usage: %s [mode] [options]\n\n"
-         "[modes]:\n"
-         "  -h / --help\t\tDisplay this help message\n"
-         "\n"
-         "  -b / --benchmark\tRun in benchmark mode\n"
-         "  [options]:\n"
-         "    -r / --iterations [number]\tNumber of iterations (default: "
-         "10)\n"
-         "\n"
-         "  -s / --sum\t\tCalculate md5sum\n"
-         "  [options]:\n"
-         "    -i / --input [file]\tInput file to read data from\n"
-         "    -o / --output [file]\tOutput file to write the md5sum to "
-         "(default: stdout)\n"
-         "\n",
-         program_name);
+  printf(
+      "Usage: %s [mode] [options]\n\n"
+      "[modes]:\n"
+      "  -h / --help\t\tDisplay this help message\n"
+      "\n"
+      "  -b / --benchmark\tRun in benchmark mode\n"
+      "  [options]:\n"
+      "    -r / --iterations [number]\tNumber of iterations (default: "
+      "10)\n"
+      "\n"
+      "  -c / --checksum\t\tCalculate md5sum\n"
+      "  [options]:\n"
+      "    -i / --input [file]\tInput file to read data from\n"
+      "    -o / --output [file]\tOutput file to write the md5sum to "
+      "(default: stdout)\n"
+      "\n"
+      "  -p / --password\t\tRun in password cracking mode\n"
+      "  [options]:\n"
+      "    -i / --input [file]\tInput file containing md5 hash\n"
+      "    -m / --pass_length [number]\tLength of the password to crack\n"
+      "    -n / --numbers\t\tInclude numbers in the password\n"
+      "    -l / --lowercase\tInclude lowercase letters in the password\n"
+      "    -u / --uppercase\tInclude uppercase letters in the password\n"
+      "    -s / --special\t\tInclude special characters in the password\n"
+      "\n",
+      program_name);
 
   exit(0);
 }
