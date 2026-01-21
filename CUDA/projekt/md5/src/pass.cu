8698cc3c9afeb3ce345988368bea119a69359f66
rafal <rafal@leja.org.pl>
2026-01-21 13:27:39 +0100

    ADD benchmark

--- CUDA/projekt/md5/src/pass.cu
+++ CUDA/projekt/md5/src/pass.cu
@@ -2,6 +2,7 @@
 #include "pass.h"
 #include "utils.h"
 #include <cstdio>
+#include <time.h>
 
 char *get_all_chars(int pass_type, int *len) {
   static char *chars = (char *)malloc(95);
@@ -42,11 +43,16 @@ bool crack_len(struct md5_state target_hash, int pass_length,
   int base = all_chars_size;
   int indices[pass_length];
   char buffer[pass_length + 1];
+  unsigned long long hash_count = 0;
+  clock_t start, end;
 
   memset(indices, 0, sizeof(indices));
   buffer[pass_length] = '\0';
 
+  start = clock();
+
   while (1) {
+    hash_count++;
     for (int i = 0; i < pass_length; i++) {
       buffer[i] = all_chars[indices[i]];
     }
@@ -56,7 +62,13 @@ bool crack_len(struct md5_state target_hash, int pass_length,
         current_hash.b == target_hash.b &&
         current_hash.c == target_hash.c &&
         current_hash.d == target_hash.d) {
+      end = clock();
       printf("Password found: %s\n", buffer);
+      printf("Time taken: %.2f seconds\n",
+             ((double)(end - start)) / CLOCKS_PER_SEC);
+      printf("Speed: %.2f million hashes/second\n",
+             hash_count / (((double)(end - start)) / CLOCKS_PER_SEC) /
+                 1e6);
       return true;
     }
 
@@ -74,6 +86,11 @@ bool crack_len(struct md5_state target_hash, int pass_length,
       break;
     }
   }
+  end = clock();
+  printf("Time taken: %.2f seconds\n",
+         ((double)(end - start)) / CLOCKS_PER_SEC);
+  printf("Speed: %.2f million hashes/second\n",
+         hash_count / (((double)(end - start)) / CLOCKS_PER_SEC) / 1e6);
   return false;
 }
 
@@ -92,6 +109,10 @@ bool crack_len_gpu(struct md5_state target_hash, int pass_length,
   char buffer[pass_length + 1];
   const int blockSize = 1024 * 1024;
   const int threadCount = 1024;
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  float elapsedTime;
 
   unsigned long long all_combinations = base;
   for (int i = 1; i < pass_length; i++) {
@@ -115,6 +136,8 @@ bool crack_len_gpu(struct md5_state target_hash, int pass_length,
 
   memset(buffer, 0, sizeof(buffer));
 
+  cudaEventRecord(start);
+
   for (unsigned long long offset = 0; offset < all_combinations;
        offset += blockSize * threadCount) {
     printf("%llu%% combinations tried...\r",
@@ -126,18 +149,38 @@ bool crack_len_gpu(struct md5_state target_hash, int pass_length,
     cudaMemcpy(&result_h, result_d, sizeof(unsigned long long),
                cudaMemcpyDeviceToHost);
     if (result_h > 0) {
+      cudaEventRecord(stop);
+      cudaEventSynchronize(stop);
+      cudaEventElapsedTime(&elapsedTime, start, stop);
+
       printf("Password found at index %llu.\n", result_h - 1);
       int_to_passwd(result_h - 1, pass_length, base, all_chars, buffer);
       printf("Password: %s\n", buffer);
+
+      printf("Time taken: %.2f seconds\n", elapsedTime / 1000.0);
+      printf("Speed: %.2f million hashes/second\n",
+             (result_h) / (elapsedTime / 1000.0) / 1e6);
+
       cudaFree(all_chars_d);
       cudaFree(target_hash_d);
       cudaFree(result_d);
+      cudaEventDestroy(start);
+      cudaEventDestroy(stop);
       return true;
     }
   }
+  cudaEventRecord(stop);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&elapsedTime, start, stop);
+  printf("Time taken: %.2f seconds\n", elapsedTime / 1000.0);
+  printf("Speed: %.2f million hashes/second\n",
+         (all_combinations) / (elapsedTime / 1000.0) / 1e6);
+
   cudaFree(all_chars_d);
   cudaFree(target_hash_d);
   cudaFree(result_d);
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
 
   return false;
 }
@@ -149,12 +192,12 @@ void crack_gpu(FILE *input_file, FILE *output_file, int pass_length,
   char *all_chars = get_all_chars(pass_type, &all_chars_size);
   for (int len = 1; len <= pass_length; len++) {
     if (pow(all_chars_size, len) < 100000) {
-      printf("Cracking passwords of length %d...\n", len);
+      printf("\nCracking passwords of length %d...\n", len);
       if (crack_len(target_hash, len, all_chars_size, all_chars)) {
         return;
       }
     } else {
-      printf("Cracking passwords of length %d on GPU...\n", len);
+      printf("\nCracking passwords of length %d on GPU...\n", len);
       if (crack_len_gpu(target_hash, len, all_chars_size, all_chars)) {
         return;
       }
@@ -169,7 +212,7 @@ void crack(FILE *input_file, FILE *output_file, int pass_length,
   char *all_chars = get_all_chars(pass_type, &all_chars_size);
 
   for (int len = 1; len <= pass_length; len++) {
-    printf("Cracking passwords of length %d...\n", len);
+    printf("\nCracking passwords of length %d...\n", len);
     if (crack_len(target_hash, len, all_chars_size, all_chars)) {
       return;
     }
